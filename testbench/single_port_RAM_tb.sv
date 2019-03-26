5e4449f558767e531bcee0d55e37da9b94d8e4c1
Yuya Kudo <kyk0910yuya@gmail.com>
2019-03-27 00:16:59 +0900

    implement testbench/single_port_RAM_tb.sv

--- testbench/single_port_RAM_tb.sv
+++ testbench/single_port_RAM_tb.sv
@@ -26,4 +26,73 @@
 // module      : single_port_RAM_tb
 // description :
 module single_port_RAM_tb();
+   localparam DATA_WIDTH    = 8;
+   localparam RAM_DEPTH     = 256;
+   localparam CLK_FREQ      = 100_000_000;
+   localparam LB_RAM_DEPTH = $clog2(RAM_DEPTH);
+
+   logic [DATA_WIDTH-1:0]   din;
+   logic [LB_RAM_DEPTH-1:0] addr;
+   logic [DATA_WIDTH-1:0]   dout;
+   logic                    wr_en;
+   logic                    clk;
+
+   //-----------------------------------------------------------------------------
+   // clock generater
+   localparam CLK_PERIOD = 1_000_000_000 / CLK_FREQ;
+
+   initial begin
+      clk = 1'b0;
+   end
+
+   always #(CLK_PERIOD / 2) begin
+      clk = ~clk;
+   end
+
+   //-----------------------------------------------------------------------------
+   // DUT
+   single_port_RAM #(DATA_WIDTH, RAM_DEPTH) dut(.din(din),
+                                                .addr(addr),
+                                                .dout(dout),
+                                                .wr_en(wr_en),
+                                                .clk(clk));
+
+   //-----------------------------------------------------------------------------
+   // test scenario
+   logic [RAM_DEPTH-1:0][DATA_WIDTH-1:0] verification_ram = '{default:0};
+   int unsigned                          max_data_val = $pow(2, DATA_WIDTH) - 1;
+   int unsigned                          val;
+
+   initial begin
+      for(int i = 0; i < RAM_DEPTH; i++) begin
+         val = $urandom_range(0, max_data_val);
+
+         // input to verification ram
+         verification_ram[i] = val;
+
+         // input to dut
+         addr  <= i;
+         din   <= val;
+         wr_en <= 1;
+
+         repeat(1) @(posedge clk);
+         wr_en <= 0;
+      end
+
+      for(int i = 0; i < RAM_DEPTH; i++) begin
+         // output from dut
+         addr  <= i;
+         wr_en <= 0;
+
+         // 2cycle delay
+         repeat(2) @(posedge clk);
+
+         repeat(1) @(posedge clk);
+         assert(dout == verification_ram[i])
+           else $error("dout is incorrect : %d", dout);
+      end
+
+      $finish;
+   end
+
 endmodule
