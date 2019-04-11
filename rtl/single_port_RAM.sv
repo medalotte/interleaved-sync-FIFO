9540ed2d7ccfe82a65aacbdb062dada11a610662
Yuya Kudo <kyk0910yuya@gmail.com>
2019-04-11 18:45:19 +0900

    update rtl/single_port_RAM.sv

--- rtl/single_port_RAM.sv
+++ rtl/single_port_RAM.sv
@@ -34,6 +34,8 @@ module single_port_RAM
      */
     DATA_WIDTH   = 8,
     RAM_DEPTH    = 256,
+
+    localparam
     LB_RAM_DEPTH = $clog2(RAM_DEPTH))
    (input  logic [DATA_WIDTH-1:0]   din,
     input  logic [LB_RAM_DEPTH-1:0] addr,
